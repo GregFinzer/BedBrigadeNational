f59d25aa72a320d2a5f2ca05d7eaf208ff81d1ab
Greg Finzer <gfinzer@hotmail.com>
2022-12-16 08:09:26 -0500

    Add Bed Brigade Tests Project

---
+++ BedBrigade.Tests/Default.cs
@@ -0,0 +1,19 @@
+namespace BedBrigade.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Keep this test for testing the pipeline
+        /// </summary>
+        [Test]
+        public void DefaultTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
