0b40341ebe291c8d35f13800909b8d9733f33c24
Greg Finzer <gfinzer@hotmail.com>
2022-12-26 10:12:14 -0500

    Create Admin Project

---
+++ BedBrigade.Admin/HandGesture.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace GrantImaharaRobotControls
+{
+    public static class HandGesture
+    {
+        public static bool ThumbUp(Axis axis, ILogger logger)
+        {
+            logger.LogInformation("Thumb up gesture. Axis: {Axis}", axis);
+
+            // Code to make robot perform gesture
+
+            return true;
+        }
+    }
+
+    public enum Axis { Left, Right }
+}
