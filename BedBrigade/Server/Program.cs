11346a86037795494491303b756166419d0a0ece
Greg Finzer <gfinzer@hotmail.com>
2023-01-01 11:02:52 -0500

    Serilog working

--- BedBrigade/Server/Program.cs
+++ BedBrigade/Server/Program.cs
@@ -1,87 +1,9 @@
-using BedBrigade.Server.Data;
-using BedBrigade.Shared;
-using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.EntityFrameworkCore;
-using Serilog;
+using BedBrigade.Server;
 
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-
-//Read logging configuration from appsettings.json
-//Log.Logger = new LoggerConfiguration()
-//    .ReadFrom.Configuration(builder.Configuration)
-//    .CreateLogger();
-
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .WriteTo.File(@"..\logs\Log_.txt", shared: true, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7)
-    .CreateLogger();
-
-Log.Logger.Information("Starting Up");
-
-// Add services to the container.
-
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-//Swagger
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-//Configure logging
-//var seriLogger = new LoggerConfiguration()
-//    .ReadFrom.Configuration(builder.Configuration)
-//    .Enrich.FromLogContext()
-//    .CreateLogger();
-//builder.Logging.AddSerilog(seriLogger);
-
-//Create database if it does not exist
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    var context = services.GetRequiredService<DataContext>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
-}
-catch (Exception ex)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occurred during migration");
-}
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+StartupLogic.ConfigureLogger(builder);
+StartupLogic.AddServicesToTheContainer(builder);
+WebApplication app = StartupLogic.CreateAndConfigureApplication(builder);
+await StartupLogic.SetupDatabase(app);
 
 app.Run();
