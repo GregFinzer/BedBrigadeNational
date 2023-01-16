38dcc33febc4f07ed84c89e2461adbe14dae6082
Mike Chafin <support@websunlimited.com>
2023-01-16 07:55:25 -0700

    Added AuthService as lifted from P. God Ecommerce

---
+++ BedBrigade/Server/Services/AuthService/AuthService.cs
@@ -0,0 +1,156 @@
+﻿using BedBrigade.Server.Data;
+using BedBrigade.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BedBrigade.Server.Services.AuthService
+{
+    public class AuthService : IAuthService
+    {
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthService(DataContext context,
+            IConfiguration configuration,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int GetUserId() => int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+        public string GetUserEmail() => _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email.ToLower().Equals(email.ToLower()));
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User not found.";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.UserName))
+            {
+                response.Success = false;
+                response.Message = "Wrong password.";
+            }
+            else
+            {
+                response.Data = CreateToken(user);
+            }
+
+            return response;
+        }
+
+        public async Task<ServiceResponse<string>> Register(User user, string password)
+        {
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<string>
+                {
+                    Data = user.UserName,
+                    Success = false,
+                    Message = "User already exists."
+                };
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = Encoding.UTF8.GetString(passwordHash);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<string> { Data = user.UserName, Message = "Registration successful!" };
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            if (await _context.Users.AnyAsync(user => user.Email.ToLower()
+                 .Equals(email.ToLower())))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, string passwordHash, string passwordSalt)
+        {
+            byte[] hash = Encoding.ASCII.GetBytes(passwordHash);
+            byte[] salt = Encoding.ASCII.GetBytes(passwordSalt);
+            using (var hmac = new HMACSHA512(salt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(hash);
+            }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        public async Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+            {
+                return new ServiceResponse<bool>
+                {
+                    Success = false,
+                    Message = "User not found."
+                };
+            }
+
+            CreatePasswordHash(newPassword, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = Encoding.ASCII.GetString(passwordHash);
+
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<bool> { Data = true, Message = "Password has been changed." };
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email.Equals(email));
+        }
+    }
+}
