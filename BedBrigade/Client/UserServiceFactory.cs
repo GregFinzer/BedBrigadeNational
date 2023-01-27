7ed8ef88cee794d7f1402cdc6bd95ce52db7f493
Mike Chafin <support@websunlimited.com>
2023-01-27 14:29:37 -0700

    UserService Factory to handle the lazyloading of the UserService

---
+++ BedBrigade/Client/UserServiceFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using BedBrigade.Shared;
+using BedBrigade.Admin.Services;
+
+namespace BedBrigade.Client
+{
+    public class UserServiceFactory : IUserServiceFactory
+    {
+        private readonly HttpClient _http;
+        private readonly AuthenticationStateProvider _authState;
+        public UserServiceFactory(HttpClient http, AuthenticationStateProvider authState)
+        {
+            _http = http;
+            _authState = authState;
+        }
+
+        public IUserService Create() => new UserService(_http, _authState);
+    }
+}
