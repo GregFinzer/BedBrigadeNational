7156b3b3e374f79b923a4d08de86462276d47394
Mike Chafin <support@websunlimited.com>
2023-01-27 06:42:20 -0700

    Changed how the authenication service is working

--- BedBrigade/Client/Services/AuthService/AuthService.cs
+++ BedBrigade/Client/Services/AuthService/AuthService.cs
@@ -2,14 +2,14 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Net.Http.Json;
 
-namespace BedBrigade.Client.Services.AuthService
+namespace BedBrigade.Client.Services
 {
-    public class AuthService : Gateway<User>, IAuthService
+    public class AuthService : IAuthService
     {
         private readonly HttpClient _http;
         private readonly AuthenticationStateProvider _authStateProvider;
 
-        public AuthService(HttpClient http, AuthenticationStateProvider authStateProvider) : base(http)
+        public AuthService(HttpClient http, AuthenticationStateProvider authStateProvider)
         {
             _http = http;
             _authStateProvider = authStateProvider;
@@ -32,10 +32,10 @@ namespace BedBrigade.Client.Services.AuthService
             return await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
         }
 
-        public async Task<ServiceResponse<int>> Register(UserRegister request)
+        public async Task<ServiceResponse<bool>> Register(UserRegister request)
         {
             var result = await _http.PostAsJsonAsync("api/auth/register", request);
-            return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
         }
     }
 }
