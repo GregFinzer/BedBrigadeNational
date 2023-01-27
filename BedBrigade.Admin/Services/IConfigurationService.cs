73d5cd6a1616d15be7d87292f4c4c60ad5a4dd68
Mike Chafin <support@websunlimited.com>
2023-01-27 06:55:33 -0700

    Added componets to Admin for Admain Manage

---
+++ BedBrigade.Admin/Services/IConfigurationService.cs
@@ -0,0 +1,12 @@
+﻿using BedBrigade.Shared;
+
+namespace BedBrigade.Admin.Services
+{
+    public interface IConfigurationService 
+    {
+        Task<ServiceResponse<string>> CreateConfigAsync(Configuration objToCreate);
+        Task<ServiceResponse<bool>> DeleteConfigAsync(string configKey);
+        Task<ServiceResponse<List<Configuration>>> GetAllAsync();
+        Task<ServiceResponse<Configuration>> UpdateAsync(Configuration configuration);
+    }
+}
