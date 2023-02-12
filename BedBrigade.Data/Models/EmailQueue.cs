558b0c31a1b68c8b32dbe1feff79a1baf1b82cd3
Mike Chafin <support@websunlimited.com>
2023-02-12 04:09:43 -0700

    Add Model

---
+++ BedBrigade.Data/Models/EmailQueue.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BedBrigade.Data.Models
+{
+    [Table("EmailQueue")]
+    public class EmailQueue
+    {
+        [Key]
+        public int EmailQueueId;
+        [Required, MaxLength(100)]
+        public string FromDisplayName { get; set; }
+        [Required, MaxLength(100)]
+        public string FromAddress { get; set; }
+        [Required, MaxLength(100)]
+        public string ToAddress { get; set; }
+        [Required, MaxLength(100)]
+        public string Subject { get; set; }
+        [Required, MaxLength(100)]
+        public string Body { get; set; }
+        [Required, MaxLength(100)]
+        public string HtmlBody { get; set; }
+        [Required]
+        public DateTime QueueDate { get; set; }
+        public DateTime LockDate { get; set; }
+        public DateTime SentDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        [Required]
+        public int Priority { get; set; }
+        [StringLength(50)]
+        public string Status { get; set; }
+        [MaxLength(4000)]
+        public string FailureMessage { get; set; }
+        [MaxLength(100)]
+        public String FirstName { get; set; }
+    }
+}
