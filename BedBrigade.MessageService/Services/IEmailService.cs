a724f931bcaa28331946d96adb9aca4a2ab91ef8
Mike Chafin <support@websunlimited.com>
2023-02-13 13:57:02 -0700

    Deleted commented out line MessageService Services

---
+++ BedBrigade.MessageService/Services/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using BedBrigade.Data.Models;
+using FluentEmail.Core.Models;
+using System.Threading.Tasks;
+
+namespace BedBrigade.MessageService.Services
+{
+    public interface IEmailService
+    {
+        Task<ServiceResponse<SendResponse>> SendEmailAsync(string To, string From, string Subject, string Template, object Model);
+    }
+}
