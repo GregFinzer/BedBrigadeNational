81ac55b9daf429ce1ef9d31afbd60d57dbb2c221
Mike Chafin <support@websunlimited.com>
2023-02-18 14:30:31 -0700

    Modified Donation to use LocationId Modified BedRequests to use LocationId Added Seeding method for Donations

---
+++ BedBrigade.Data/Migrations/20230218204027_ChangeLocationInBedRequest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BedBrigade.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLocationInBedRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VolunteerForId",
+                table: "Volunteers",
+                newName: "VolunteeringForId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Volunteers_VolunteerForId",
+                table: "Volunteers",
+                newName: "IX_Volunteers_VolunteeringForId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VolunteeringForId",
+                table: "Volunteers",
+                newName: "VolunteerForId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Volunteers_VolunteeringForId",
+                table: "Volunteers",
+                newName: "IX_Volunteers_VolunteerForId");
+        }
+    }
+}
