c414c1f7855e80be11228d80feac7567194fe373
Val Skordin <vskordin@gmail.com>
2023-02-19 13:05:28 -0800

    1) media.cs - media data model modified - FileSize type changed to int 2) DataContextModelSnapshot.cs - modified media table 3) seeds.cs - removed population of media table (except one row) 4) seeds.cs - added media items to configuration table 5) added Media Data Services & Media Services 6) StartUpLogic.cs - added Media Services registration 7) _host.cshtml - added link to FontAwesome 4.7 CDN

---
+++ BedBrigade.Client/Services/MediaService.cs
@@ -0,0 +1,44 @@
+﻿
+using BedBrigade.Data.Models;
+using BedBrigade.Data.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BedBrigade.Client.Services
+{
+    public class MediaService : IMediaService
+    {
+        private readonly AuthenticationStateProvider _authState;
+        private readonly IMediaDataService _data;
+
+        public MediaService(AuthenticationStateProvider authState, IMediaDataService dataService)
+        {
+            _authState = authState;
+            _data = dataService;
+        }
+
+        public async Task<ServiceResponse<bool>> DeleteAsync(int mediaId) // delete media by ID
+        {
+            return await _data.DeleteAsync(mediaId);
+        } // delete media
+
+        public async Task<ServiceResponse<Media>> GetAsync(int mediaId) // get single media record by ID
+        {
+            return await _data.GetAsync(mediaId);
+        } // get singl;e media record
+
+        public async Task<ServiceResponse<List<Media>>> GetAllAsync() // get all media records
+        {
+            return await _data.GetAllAsync();
+        } // get all media records
+
+        public async Task<ServiceResponse<Media>> UpdateAsync(Media media) // update media record (object)
+        {
+            return await _data.UpdateAsync(media);
+        } // update media record
+
+        public async Task<ServiceResponse<Media>> CreateAsync(Media media) // add new media
+        {
+            return await _data.CreateAsync(media);
+        } // add new media
+    } //end MediaService class
+} // namespace
