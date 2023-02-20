351ad69e46c52f64ded485abdd8e30cd49e8f857
Mike Chafin <support@websunlimited.com>
2023-02-20 16:57:15 -0700

    Added migration for lookup table VolunteerFor

---
+++ BedBrigade.Data/Migrations/20230218111828_AddVolunteerForTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BedBrigade.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVolunteerForTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VolunteersFor",
+                columns: table => new
+                {
+                    VolunteerForId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VolunteersFor", x => x.VolunteerForId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VolunteersFor");
+        }
+    }
+}
