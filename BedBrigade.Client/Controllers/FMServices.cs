ed40509c01fd0111f50aa2bdf670c17ce15857d4
Mike Chafin <support@websunlimited.com>
2023-03-15 07:24:59 -0700

    Codes to add Syncfusion File Manager

---
+++ BedBrigade.Client/Controllers/FMServices.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Syncfusion.Blazor.FileManager;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+using FileManagerDirectoryContent = Syncfusion.Blazor.FileManager.Base.FileManagerDirectoryContent;
+
+namespace BedBrigade.Client.Controllers
+{
+
+    [Route("[controller]")]
+    public class FileManagerController : Controller
+    {
+        public PhysicalFileProvider operation;
+        public string basePath;
+
+        [Obsolete]
+        public FileManagerController(IHostingEnvironment hostingEnvironment)
+        {
+            basePath = hostingEnvironment.ContentRootPath;
+            operation = new PhysicalFileProvider();
+            operation.RootFolder(basePath + "\\wwwroot\\Media"); // Data\\Files denotes in which files and folders are available.
+        }
+
+
+        // Processing the File Manager operations
+        [Route("FileOperations")]
+        public object FileOperations([FromBody] FileManagerDirectoryContent args)
+        {
+            switch (args.Action)
+            {
+                // Add your custom action here
+                case "read":
+                    // Path - Current path; ShowHiddenItems - Boolean value to show/hide hidden items
+                    return operation.ToCamelCase(operation.GetFiles(args.Path, args.ShowHiddenItems));
+                case "delete":
+                    // Path - Current path where of the folder to be deleted; Names - Name of the files to be deleted
+                    return operation.ToCamelCase(operation.Delete(args.Path, args.Names));
+                case "copy":
+                    //  Path - Path from where the file was copied; TargetPath - Path where the file/folder is to be copied; RenameFiles - Files with same name in the copied location that is confirmed for renaming; TargetData - Data of the copied file
+                    return operation.ToCamelCase(operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "move":
+                    // Path - Path from where the file was cut; TargetPath - Path where the file/folder is to be moved; RenameFiles - Files with same name in the moved location that is confirmed for renaming; TargetData - Data of the moved file
+                    return operation.ToCamelCase(operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "details":
+                    // Path - Current path where details of file/folder is requested; Name - Names of the requested folders
+                    return operation.ToCamelCase(operation.Details(args.Path, args.Names));
+                case "create":
+                    // Path - Current path where the folder is to be created; Name - Name of the new folder
+                    return operation.ToCamelCase(operation.Create(args.Path, args.Name));
+                case "search":
+                    // Path - Current path where the search is performed; SearchString - String typed in the searchbox; CaseSensitive - Boolean value which specifies whether the search must be casesensitive
+                    return operation.ToCamelCase(operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
+                case "rename":
+                    // Path - Current path of the renamed file; Name - Old file name; NewName - New file name
+                    return operation.ToCamelCase(operation.Rename(args.Path, args.Name, args.NewName));
+            }
+            return null;
+        }
+        [Route("Download")]
+        public IActionResult Download(string downloadInput)
+        {
+            //Invoking download operation with the required paramaters
+            // path - Current path where the file is downloaded; Names - Files to be downloaded;
+            FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
+            return operation.Download(args.Path, args.Names);
+        }
+        [Route("Upload")]
+        public IActionResult Upload(string path, IList<IFormFile> uploadFiles, string action)
+        {
+            //Invoking upload operation with the required paramaters
+            // path - Current path where the file is to uploaded; uploadFiles - Files to be uploaded; action - name of the operation(upload)
+
+            var uploadResponse = operation.Upload(path, uploadFiles, action, null);
+            if (uploadResponse.Error != null)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+                Response.HttpContext.Features.Get<Microsoft.AspNetCore.Http.Features.IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
+            }
+            return Content("");
+        }
+        [Route("GetImage")]
+        public IActionResult GetImage(FileManagerDirectoryContent args)
+        {
+            //Invoking GetImage operation with the required paramaters
+            // path - Current path of the image file; Id - Image file id;
+            return operation.GetImage(args.Path, args.Id, false, null, null);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
