38925e7ca314d0e2b1bd9f14a3c956addb3ad6dc
Mike Chafin <support@websunlimited.com>
2023-06-01 06:24:04 -0700

    Conflict and change message

--- BedBrigade.Data/Models/UserRegister.cs
+++ BedBrigade.Data/Models/UserRegister.cs
@@ -5,7 +5,7 @@ namespace BedBrigade.Data.Models
     public class UserRegister
     {
         public User user { get; set; }
-        [StringLength(100, MinimumLength = 6)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "6 characters minimum.")]
         [Required(ErrorMessage = "New password is required")]
         public string Password { get; set; } = string.Empty;
         [Compare("Password", ErrorMessage = "The passwords do not match.")]
