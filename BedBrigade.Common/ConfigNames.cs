67c5421f713ed46af445970aeb25a4c45f63097c
Greg Finzer <gfinzer@hotmail.com>
2023-06-05 12:53:43 -0400

    Add config names

---
+++ BedBrigade.Common/ConfigNames.cs
@@ -0,0 +1,17 @@
+﻿namespace BedBrigade.Common
+{
+    public static class ConfigNames
+    {
+        public const string FromEmailAddress = "FromEmailAddress";
+        public const string HostName = "HostName";
+        public const string Port = "Port";
+        public const string TokenExpiration = "TokenExpiration";
+        public const string AllowedFileExtensions = "AllowedFileExtensions";
+        public const string AllowedVideoExtensions = "AllowedVideoExtensions";
+        public const string MediaFolder = "MediaFolder";
+        public const string MainMediaSubFolder = "MainMediaSubFolder";
+        public const string MaxFileSize = "MaxFileSize";
+        public const string MaxVideoSize = "MaxVideoSize";
+        public const string IsCachingEnabled = "IsCachingEnabled";
+    }
+}
