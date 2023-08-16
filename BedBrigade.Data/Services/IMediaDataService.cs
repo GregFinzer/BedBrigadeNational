f7edd0688f7ad92b337f1bc880bb1588efd2498b
Greg Finzer <gfinzer@hotmail.com>
2023-08-16 15:46:48 -0400

    Media Repository

--- BedBrigade.Data/Services/IMediaDataService.cs
+++ BedBrigade.Data/Services/IMediaDataService.cs
@@ -2,13 +2,7 @@
 
 namespace BedBrigade.Data.Services
 {
-    public interface IMediaDataService
+    public interface IMediaDataService : IRepository<Media>
     {
-        Task<ServiceResponse<Media>> CreateAsync(Media media);
-        Task<ServiceResponse<bool>> DeleteAsync(int MediaId);
-        Task<ServiceResponse<List<Media>>> GetAllAsync();
-        Task<ServiceResponse<Media>> GetAsync(int MediaId);
-        Task<ServiceResponse<Media>> UpdateAsync(Media media);
-        Task<ServiceResponse<List<Media>>> GetLogos();
     }
 }
--- BedBrigade.Data/Services/MediaDataService.cs
+++ BedBrigade.Data/Services/MediaDataService.cs
@@ -1,101 +1,15 @@
 ﻿using BedBrigade.Data.Models;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.EntityFrameworkCore;
-using System.Data.Common;
 
 namespace BedBrigade.Data.Services;
 
-public class MediaDataService : IMediaDataService
+public class MediaDataService :  Repository<Media>, IMediaDataService
 {
-    private readonly DataContext _context;
-
-    public MediaDataService(DataContext context)
+    public MediaDataService(IDbContextFactory<DataContext> contextFactory, ICachingService cachingService, AuthenticationStateProvider authProvider) : base(contextFactory, cachingService, authProvider)
     {
-        _context = context;
     }
-
-    public async Task<ServiceResponse<Media>> GetAsync(int mediaId) // get single media recird by ID
-    {
-        var result = await _context.Media.FindAsync(mediaId);
-        if (result != null)
-        {
-            return new ServiceResponse<Media>("Found Media Record", true, result);
-        }
-        return new ServiceResponse<Media>("Media Record Not Found");
-    } // Get record
-
-    public async Task<ServiceResponse<List<Media>>> GetAllAsync() // Get all Media records (table "Media")
-    {
-        var result = await _context.Media.ToListAsync();
-        if (result != null)
-        {
-            return new ServiceResponse<List<Media>>($"Found {result.Count} media records.", true, result);
-        }
-        return new ServiceResponse<List<Media>>("None Media records found.");
-    } // Get All Records
-
-    public async Task<ServiceResponse<bool>> DeleteAsync(int mediaId) // Delete media by ID
-    {
-        var myMedia = await _context.Media.FindAsync(mediaId);
-        if (myMedia == null)
-        {
-            return new ServiceResponse<bool>($"Media record with MediaId = {mediaId} not found");
-        }
-        try
-        {
-            _context.Media.Remove(myMedia);
-            await _context.SaveChangesAsync();
-            return new ServiceResponse<bool>($"Removed media record with ID = {mediaId}.", true);
-        }
-        catch (DbException ex)
-        {
-            return new ServiceResponse<bool>($"Database error on delete of media record with ID = {mediaId} - {ex.Message} ({ex.ErrorCode})");
-        }
-    } // Delete media
-
-    public async Task<ServiceResponse<Media>> UpdateAsync(Media media) // update Media record (object)
-    {
-        var result = await Task.Run(() =>_context.Media.Update(media));
-        await _context.SaveChangesAsync();
-        if (result != null)
-        {
-            return new ServiceResponse<Media>($"Updated Media with ID = {media.MediaId}", true);
-        }
-        return new ServiceResponse<Media>($"Media with ID = {media.MediaId} was not updated.");
-    } // update media record
-
-    public async Task<ServiceResponse<Media>> CreateAsync(Media media) // add new media object
-    {
-        try
-        {
-            await _context.Media.AddAsync(media);
-            await _context.SaveChangesAsync();
-            return new ServiceResponse<Media>($"Added new Media with ID =  {media.MediaId}", true);
-        }
-        catch (DbException ex)
-        {
-            return new ServiceResponse<Media>($"Database error: new  media cannot be added - {ex.Message} ({ex.ErrorCode})");
-        }
-    } // add new media
-
-    public async Task<ServiceResponse<List<Media>>> GetLogos()
-    {
-        List<Media> result;
-        try
-        {
-            result = await _context.Media.Where(m => m.FileUse == Common.Common.FileUse.Logo).ToListAsync();
-        }
-        catch (Exception ex)
-        {
-            return new ServiceResponse<List<Media>>($"Error at media logo fetch - {ex.Message}");
-        }
-        if (result != null)
-        {
-            return new ServiceResponse<List<Media>>($"Found {result.Count} media records.", true, result);
-        }
-        return new ServiceResponse<List<Media>>("None Media records found.");
-
-    }
-} // end class MediaDataService
+} 
 
 
 
