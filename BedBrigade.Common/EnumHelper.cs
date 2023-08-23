f33de72f513452c5f28793a711440887e8f1fde2
Greg Finzer <gfinzer@hotmail.com>
2023-08-23 15:03:19 -0400

    Manage ContactUs in progress

---
+++ BedBrigade.Common/EnumHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace BedBrigade.Common
+{
+    public static class EnumHelper
+    {       
+        public static string GetEnumDescription<T>(T value) where T : Enum
+        {
+            var fi = value.GetType().GetField(value.ToString());
+            var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : value.ToString();
+        }
+
+        public static List<EnumNameValue<T>> GetEnumNameValues<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>().Select(e => new EnumNameValue<T> { Value = e, Name = GetEnumDescription(e) }).ToList();
+        }
+    }
+}
---
+++ BedBrigade.Common/EnumNameValue.cs
@@ -0,0 +1,8 @@
+﻿namespace BedBrigade.Common
+{
+    public class EnumNameValue<T> where T : Enum
+    {
+        public T Value { get; set; }
+        public string Name { get; set; }
+    }
+}
