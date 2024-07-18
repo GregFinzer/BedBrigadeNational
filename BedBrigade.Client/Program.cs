7d5d2428dc4e01e9d9e1a0b25547b7f115fcc9af
Greg Finzer <gfinzer@hotmail.com>
2024-07-18 07:58:32 -0400

    Blazor 8 with just test files

--- BedBrigade.Client/Program.cs
+++ BedBrigade.Client/Program.cs
@@ -1,11 +1,27 @@
-using BedBrigade.Client;
+using BedBrigade.Client.Components;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-StartupLogic.ConfigureLogger(builder);
-StartupLogic.AddServicesToTheContainer(builder);
-WebApplication app = StartupLogic.CreateAndConfigureApplication(builder);
-await StartupLogic.SetupDatabase(app);
-await StartupLogic.SetupCaching(app);
-StartupLogic.SetupEmailQueueProcessing(app);
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
 app.Run();
-
