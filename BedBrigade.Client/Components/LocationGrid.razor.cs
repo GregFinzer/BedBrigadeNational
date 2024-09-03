dee730b5d105d4b81a602b0ae3a40108b194dd6c
Greg Finzer <gfinzer@hotmail.com>
2024-09-03 10:56:17 -0400

    Only Copy Media for Specific Pages for Add Location

--- BedBrigade.Client/Components/LocationGrid.razor.cs
+++ BedBrigade.Client/Components/LocationGrid.razor.cs
@@ -246,9 +246,6 @@ namespace BedBrigade.Client.Components
                 await AddNewLocationAsync(Location);
             }
 
-            // validate location folders
-            var bLocationFolderStatus = FileUtil.CreateOrValidateLocationFolders(Location.Route); // VS 8/25/2023
-
             await Grid.CallStateHasChangedAsync();
             await Grid.Refresh();
         }
