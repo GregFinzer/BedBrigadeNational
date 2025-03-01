58704c9931ebe5ef2bb6adfc730f166ff5afc4f3
Greg Finzer <gfinzer@hotmail.com>
2025-03-01 14:38:53 -0500

    Missing EmailSlim

---
+++ BedBrigade.Common/Models/EmailSlim.cs
@@ -0,0 +1,9 @@
+﻿namespace BedBrigade.Common.Models
+{
+    public class EmailSlim
+    {
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public DateTime? SentDate { get; set; }
+    }
+}
