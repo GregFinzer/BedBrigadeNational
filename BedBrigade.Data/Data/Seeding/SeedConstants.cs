fbfb81447871b2acafcd8fb6875197a08d8bf19f
Greg Finzer <gfinzer@hotmail.com>
2025-08-05 15:24:00 -0400

    Rename Rock City Polaris to Polaris

--- BedBrigade.Data/Data/Seeding/SeedConstants.cs
+++ BedBrigade.Data/Data/Seeding/SeedConstants.cs
@@ -6,6 +6,6 @@ namespace BedBrigade.Data.Data.Seeding
         public const string SeedUserName = "Seed";
         public const string SeedNationalName = "National";
         public const string SeedGroveCityName = "GroveCity";
-        public const string SeedRockCityPolarisName = "RockCityPolaris";
+        public const string SeedPolarisName = "Polaris";
     }
 }
