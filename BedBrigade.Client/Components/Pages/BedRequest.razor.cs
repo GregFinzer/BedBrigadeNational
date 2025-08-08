01881fcf93427bc212de39b5140a2dd5735b15ea
Greg Finzer <gfinzer@hotmail.com>
2025-08-08 19:03:58 -0400

    Fix for quality and localization

--- BedBrigade.Client/Components/Pages/BedRequest.razor.cs
+++ BedBrigade.Client/Components/Pages/BedRequest.razor.cs
@@ -7,7 +7,6 @@ using BedBrigade.Common.Models;
 using BedBrigade.Data.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.JSInterop;
 using Serilog;
 using Syncfusion.Blazor.DropDowns;
@@ -445,23 +444,8 @@ namespace BedBrigade.Client.Components.Pages
         {
             try
             {
-                //Set it to the primary city name
-                newRequest.City = Validation.GetCityForZipCode(newRequest.PostalCode);
-                newRequest.Reference = "Website";
-                Common.Models.BedRequest bedRequest = new Common.Models.BedRequest();
-                ObjectUtil.CopyProperties(newRequest, bedRequest);
-                string defaultNote = await _svcConfiguration.GetConfigValueAsync(ConfigSection.CustomStrings,
-                    ConfigNames.BedRequestNote, bedRequest.LocationId);
+                Common.Models.BedRequest? bedRequest = await BuildBedRequest();
 
-                if (String.IsNullOrWhiteSpace(newRequest.SpecialInstructions))
-                {
-                    bedRequest.Notes = defaultNote;
-                }
-                else
-                {
-                    bedRequest.Notes = defaultNote + " " + newRequest.SpecialInstructions;
-                }
-            
                 var addResult = await _svcBedRequest.CreateAsync(bedRequest);
                 if (addResult.Success && addResult.Data != null)
                 {
@@ -498,6 +482,28 @@ namespace BedBrigade.Client.Components.Pages
             }
 
             return null;
+        }
+
+        private async Task<Common.Models.BedRequest> BuildBedRequest()
+        {
+            //Set it to the primary city name
+            newRequest.City = Validation.GetCityForZipCode(newRequest.PostalCode);
+            newRequest.Reference = "Website";
+            Common.Models.BedRequest bedRequest = new Common.Models.BedRequest();
+            ObjectUtil.CopyProperties(newRequest, bedRequest);
+            string defaultNote = await _svcConfiguration.GetConfigValueAsync(ConfigSection.CustomStrings,
+                ConfigNames.BedRequestNote, bedRequest.LocationId);
+
+            if (String.IsNullOrWhiteSpace(newRequest.SpecialInstructions))
+            {
+                bedRequest.Notes = defaultNote;
+            }
+            else
+            {
+                bedRequest.Notes = defaultNote + " " + newRequest.SpecialInstructions;
+            }
+
+            return bedRequest;
         }
 
         #endregion
