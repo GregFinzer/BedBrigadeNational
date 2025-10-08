4000d9f0cc7819f105b94f324c195ad9e3754a8d
Greg Finzer <gfinzer@hotmail.com>
2025-10-08 09:14:51 -0400

    Dashboard Schedules

--- BedBrigade.Client/Components/Pages/Administration/Dashboard.razor.cs
+++ BedBrigade.Client/Components/Pages/Administration/Dashboard.razor.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BedBrigade.Data.Services;
+using Microsoft.AspNetCore.Components;
+using BedBrigade.Common.Models;
 
 namespace BedBrigade.Client.Components.Pages.Administration
 {
     public partial class Dashboard : ComponentBase
     {
+        [Inject] protected IScheduleDataService ScheduleService { get; set; } = default!;
+        [Inject] protected IAuthService AuthService { get; set; } = default!;
+
+        protected List<Common.Models.Schedule>? Schedules { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            int locationId = AuthService.LocationId;
+            var response = await ScheduleService.GetScheduleForMonthsAndLocation(locationId, 3);
+            if (response.Success)
+            {
+                Schedules = response.Data;
+            }
+            else
+            {
+                Schedules = new List<Common.Models.Schedule>();
+            }
+        }
     }
 }
 
