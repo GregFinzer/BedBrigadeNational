58027e74c7a187c6f3069dfe6bb9dbf749d9ef7b
Greg Finzer <gfinzer@hotmail.com>
2026-08-16 07:36:14 -0400

    Include scheduled with sorting

--- BedBrigade.Data/Services/BedRequestDataService.cs
+++ BedBrigade.Data/Services/BedRequestDataService.cs
@@ -420,23 +420,16 @@ public class BedRequestDataService : Repository<BedRequest>, IBedRequestDataServ
             }
 
             targetBedRequest.Distance = -1;
-            
-            var waitingRequests = bedRequests
-                .Where(b => b.BedRequestId != targetBedRequest.BedRequestId && b.Status == BedRequestStatus.Waiting)
+
+            List<BedRequest> waitingOrScheduled = bedRequests
+                .Where(b => b.BedRequestId != targetBedRequest.BedRequestId 
+                    && (b.Status == BedRequestStatus.Waiting || b.Status == BedRequestStatus.Scheduled))
                 .ToList();
             
-            var routeOrderedWaitingRequests = OrderByBestRoute(
-                waitingRequests,
+            return OrderByBestRoute(
+                waitingOrScheduled,
                 targetBedRequest.Latitude.HasValue ? (double?)targetBedRequest.Latitude.Value : null,
                 targetBedRequest.Longitude.HasValue ? (double?)targetBedRequest.Longitude.Value : null);
-            
-            var result = new List<BedRequest> { targetBedRequest };
-            result.AddRange(routeOrderedWaitingRequests);
-
-            var scheduledRequests = bedRequests.Where(o => o.Status == BedRequestStatus.Scheduled
-                                                           && o.BedRequestId != targetBedRequest.BedRequestId);
-            result.AddRange(scheduledRequests);
-            return result;
         }
         catch (Exception ex)
         {
